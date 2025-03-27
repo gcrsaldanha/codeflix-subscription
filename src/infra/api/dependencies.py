ac87a466f759de4461d8c71d500d2ff6d733cb7e
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2025-03-27 08:57:56 -0300

    6.2 Subscription API and dependencies

--- src/infra/api/dependencies.py
+++ src/infra/api/dependencies.py
@@ -5,16 +5,28 @@ from sqlmodel import Session
 
 from src.application.create_plan import CreatePlanUseCase
 from src.application.create_user_account import CreateUserAccountUseCase
-from src.domain.repositories import PlanRepository, UserAccountRepository
+from src.application.subscribe_to_plan import SubscribeToPlanUseCase
+from src.domain.repositories import (
+    PlanRepository,
+    UserAccountRepository,
+    SubscriptionRepository,
+)
 from src.infra.auth import AuthService
+from src.infra.auth.in_memory_auth_service import InMemoryAuthService
 from src.infra.db import (
     get_session,
     SQLModelPlanRepository,
     SQLModelUserAccountRepository,
 )
-from src.tests.fixtures.infra.repositories.in_memory_auth_service import (
-    InMemoryAuthService,
+from src.infra.db.repositories.sql_model_subscription_repository import (
+    SQLModelSubscriptionRepository,
 )
+from src.infra.notification.console_notification_service import (
+    ConsoleNotificationService,
+)
+from src.infra.notification.notification_service import NotificationService
+from src.infra.payment.fake_payment_gateway import FakePaymentGateway
+from src.infra.payment.payment_gateway import PaymentGateway
 
 # Database
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -29,9 +41,16 @@ def get_user_account_repository(session: SessionDep) -> UserAccountRepository:
     return SQLModelUserAccountRepository(session)
 
 
+def get_subscription_repository(session: SessionDep) -> SubscriptionRepository:
+    return SQLModelSubscriptionRepository(session)
+
+
 PlanRepositoryDep = Annotated[PlanRepository, Depends(get_plan_repository)]
 UserAccountRepositoryDep = Annotated[
     UserAccountRepository, Depends(get_user_account_repository)
+]
+SubscriptionRepositoryDep = Annotated[
+    SubscriptionRepository, Depends(get_subscription_repository)
 ]
 
 
@@ -40,7 +59,19 @@ def get_auth_service() -> AuthService:
     return InMemoryAuthService()  # TODO: replace by KeycloakAuthService
 
 
+def get_notification_service() -> NotificationService:
+    return ConsoleNotificationService()  # TODO: EmailNotificationService
+
+
+def get_payment_gateway() -> PaymentGateway:
+    return FakePaymentGateway()
+
+
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
+NotificationServiceDep = Annotated[
+    NotificationService, Depends(get_notification_service)
+]
+PaymentGatewayDep = Annotated[PaymentGateway, Depends(get_payment_gateway)]
 
 
 # Use cases
@@ -55,7 +86,26 @@ def get_create_user_account_use_case(
     return CreateUserAccountUseCase(auth_service, user_account_repository)
 
 
+def get_subscribe_to_plan_use_case(
+    payment_gateway: PaymentGatewayDep,
+    notification_service: NotificationServiceDep,
+    subscription_repository: SubscriptionRepositoryDep,
+    user_account_repository: UserAccountRepositoryDep,
+    plan_repository: PlanRepositoryDep,
+) -> SubscribeToPlanUseCase:
+    return SubscribeToPlanUseCase(
+        payment_gateway,
+        notification_service,
+        subscription_repository,
+        user_account_repository,
+        plan_repository,
+    )
+
+
 CreatePlanUseCaseDep = Annotated[CreatePlanUseCase, Depends(get_create_plan_use_case)]
 CreateUserAccountUseCaseDep = Annotated[
     CreateUserAccountUseCase, Depends(get_create_user_account_use_case)
+]
+SubscribeToPlanUseCaseDep = Annotated[
+    SubscribeToPlanUseCase, Depends(get_subscribe_to_plan_use_case)
 ]
--- src/infra/api/fastapi.py
+++ src/infra/api/fastapi.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.infra.api.routes import plans_router, accounts_router
+from src.infra.api.routes import plans_router, accounts_router, subscriptions_router
 from src.infra.db import create_db_and_tables
 
 
@@ -19,6 +19,7 @@ app = FastAPI(title="Subscription Service API", lifespan=lifespan)
 # Include routers
 app.include_router(plans_router)
 app.include_router(accounts_router)
+app.include_router(subscriptions_router)
 
 if __name__ == "__main__":
     import uvicorn

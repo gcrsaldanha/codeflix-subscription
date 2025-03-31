7f081a7dc841bb83c25d64582039773a7ac45ab3
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2025-03-30 21:05:52 -0300

    9.2 Docker: PosgreSQL

--- src/infra/db/__init__.py
+++ src/infra/db/__init__.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlmodel import Session, SQLModel, create_engine
 
 from .repositories.sql_model_plan_repository import SQLModelPlanRepository
 from .repositories.sql_model_user_account_repository import SQLModelUserAccountRepository
 
-DATABASE_URL = "sqlite:///./subscription_service.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./subscription_service.db")
 engine = create_engine(DATABASE_URL)
 
 
